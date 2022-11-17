c1956070e829964f99b06965183af8f2fca23fa9
unknown <lth4904@naver.com>
2022-11-17 17:28:46 +0900

    user database 수정

--- routers/user.py
+++ routers/user.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter, HTTPException, Depends
 from dependencies import get_auth, get_db
 from repositories import user_repository
 from schemas import user_schema
+from sqlalchemy.orm import Session
 
 router = APIRouter(
     prefix="/users",
@@ -11,17 +12,17 @@ router = APIRouter(
     responses={404: {"description": "Not Found"}},
 )
 
-fake_users_db = {"1": {"name": "박재현"}, "2": {"name": "조유신"}}
-
 
 @router.post("/", response_model=user_schema.User)
-async def create_user(user: user_schema.UserCreate, db=Depends(get_db)):
+async def create_user(user: user_schema.UserCreate, db: Session = Depends(get_db)):
     return user_repository.create_user(db, user)
 
 
-@router.get("/")
-async def read_items():
-    return fake_users_db
+@router.get("/", response_model=list[user_schema.User])
+async def read_items(skip: int = 0, limit: int = 100,
+                     db: Session = Depends(get_db)):
+    users = user_repository.get_users(db, skip=skip, limit=limit)
+    return users
 
 
 @router.get("/me")
@@ -29,8 +30,7 @@ async def me(user: str = Depends(get_auth)):
     return user
 
 
-@router.get("/{user_id}")
-async def read_item(user_id: str):
-    if user_id not in fake_users_db:
-        raise HTTPException(status_code=404, detail="Item not found")
-    return {"name": fake_users_db[user_id]["name"], "user_id": user_id}
+@router.get("/{user_id}", response_model=user_schema.User)
+async def read_item(user_id: int, db:Session = Depends(get_db)):
+    user = user_repository.get_user(db, user_id)
+    return user

6edc0387215ad9a42c442df40ef2233642ebddf1
nooreong2 <97031069+nooreong2@users.noreply.github.com>
2022-11-30 09:07:32 +0900

    face score 점수 산정 방식 변경

--- services/face_service.py
+++ services/face_service.py
@@ -8,9 +8,8 @@ from services.sentiment_analyzing import Face_DTO
 
 def create_faces(faces: list[Face_DTO], video_model: Video, db: Session):
     face_sch_list = list(
-        map(lambda f: face_schema.FaceCreate(anger_score=f.anger * 20, joy_score=f.joy * 20,
-                                             sorrow_score=f.sorrow * 20,
-                                             surprised_score=f.surprise * 20, video_id=video_model.id), faces)
+        map(lambda f: face_schema.FaceCreate(anger_score=f.anger, joy_score=f.joy,
+                                             sorrow_score=f.sorrow,
+                                             surprised_score=f.surprise, video_id=video_model.id), faces)
     )
-    print(face_sch_list)
     face_repository.create_faces(db, face_sch_list)
